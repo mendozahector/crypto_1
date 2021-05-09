f83afbb4543c0e4a359632054e5a9d87a0f8d1ee
Hector <hectormendoza@byui.edu>
2021-05-08 22:40:44 -0600

    added json files

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-class Crypto:
-    def __init__(self, name, acronym, amount):
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+class Currency:
+    def __init__(self, name, symbol, amount):
         self.name = name
-        self.acronym = acronym
+        self.symbol = symbol
         self.amount = amount
 
 def digital_wallet():
-    cryptos = list()
+    wallet = list()
 
-    dollars = Crypto('Dollars', 'USD', 4577)
-    bitcoin = Crypto('Bitcoin', 'BTC', 3.578)
-    ethereum = Crypto('Ethereum', 'ETH', 1.23)
-    dogecoin = Crypto('Dogecoin', 'DOGE', 12.245)
+    dollars = Currency('Dollars', 'USD', 4577)
+    bitcoin = Currency('Bitcoin', 'BTC', 3.578)
+    ethereum = Currency('Ethereum', 'ETH', 1.23)
+    dogecoin = Currency('Dogecoin', 'DOGE', 12.245)
 
-    cryptos.append(dollars)
-    cryptos.append(bitcoin)
-    cryptos.append(ethereum)
-    cryptos.append(dogecoin)
+    wallet.append(dollars)
+    wallet.append(bitcoin)
+    wallet.append(ethereum)
+    wallet.append(dogecoin)
 
     def print_money():
         print("Your current digital wallet:\n")
 
-        for crypto in cryptos:
-            print(f"{crypto.name} - {crypto.acronym}: {crypto.amount}")
+        for currency in wallet:
+            print(f"{currency.name} - {currency.symbol}: {currency.amount}")
 
     menu = """\nPlease select an option:
     1. Deposit
@@ -39,16 +43,20 @@ def digital_wallet():
                 break
             elif choice == 1:
                 deposit = float(input("Please enter your amount in dollars (USD): "))
-                print(f"We have added ${deposit} to your wallet. Thank you.\n")
-                cryptos[0].amount += deposit
+
+                if deposit > 0:
+                    print(f"We have added ${deposit} to your wallet. Thank you.\n")
+                    wallet[0].amount += deposit
+                else:
+                    print("Please enter an amount greater than zero.")
 
                 print_money()
                 print(menu)
             elif choice == 2:
-                withdraw = float(input(f"Enter the amount to withdraw (up to ${cryptos[0].amount}): "))
+                withdraw = float(input(f"Enter the amount to withdraw (up to ${wallet[0].amount}): "))
 
-                if withdraw > 0 and withdraw <= cryptos[0].amount:
-                    cryptos[0].amount -= withdraw
+                if withdraw > 0 and withdraw <= wallet[0].amount:
+                    wallet[0].amount -= withdraw
                     print("Success. Please allow 2-3 business days for transaction to process.")
                 else:
                     print("Invalid withdraw amount. Please try again.\n")
@@ -62,8 +70,33 @@ def digital_wallet():
             print("Invalid option. Please try again.")
 
 def crypto_charts():
-    print("Displaying crypto charts...\n")
+    print("Retrieving crypto data...\n")
 
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+    'start':'1',
+    'limit':'10',
+    'convert':'USD'
+    }
+    headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '7aff45c2-193a-46b5-8ac8-9d4a15b27879',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        
+        for crypto in data['data']:
+            print(f"* {crypto['name']} - {crypto['symbol']}: $ {crypto['quote']['USD']['price']:.4f}")
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+    print()
+    
 def crypto_trading():
     print("Ready to trade?\n")
 
@@ -86,6 +119,7 @@ while choice != 0:
             print(menu)
         elif choice == 2:
             crypto_charts()
+            print(menu)
         elif choice == 3:
             crypto_trading()
         else:
